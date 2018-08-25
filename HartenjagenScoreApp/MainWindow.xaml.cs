73e5da5d7acd5a0be8b7706fc64b0611afac25d5
Marc Löwenstein <TheStoneLion67@gmail.com>
2018-08-25 15:22:24 +0200

    Uitwerekn design

--- HartenjagenScoreApp/MainWindow.xaml.cs
+++ HartenjagenScoreApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,43 @@ namespace HartenjagenScoreApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ProcessRoundButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void DeleteLastRoundButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Weet je het zeker?", "Verwijderen laatste ronde", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+        }
+
+        private void StartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Weet je het zeker?", "Nieuw spel starten", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+        }
+
+        private void RondeScoreWaardeSpeler1_DataContextChanged(object sender, RoutedEventArgs e)
+        {
+            this.SpelInfoRest.Text = "1";
+        }
+
+        private void RondeScoreWaardeSpeler2_DataContextChanged(object sender, RoutedEventArgs e)
+        {
+            this.SpelInfoRest.Text = "2";
+        }
+        private void RondeScoreWaardeSpeler3_DataContextChanged(object sender, RoutedEventArgs e)
+        {
+            this.SpelInfoRest.Text = "3";
+        }
+        private void RondeScoreWaardeSpeler4_DataContextChanged(object sender, RoutedEventArgs e)
+        {
+            this.SpelInfoRest.Text = "4";
+        }
+
+        private void RondeScoreWaardeSpeler1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
         }
     }
 }
