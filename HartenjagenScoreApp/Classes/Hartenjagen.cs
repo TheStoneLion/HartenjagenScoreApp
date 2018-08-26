13fcaf8b20340d735fa9673cc615e1773ec238df
Marc Löwenstein <TheStoneLion67@gmail.com>
2018-08-26 09:58:48 +0200

    Updates zaterdag

--- HartenjagenScoreApp/Classes/Hartenjagen.cs
+++ HartenjagenScoreApp/Classes/Hartenjagen.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HartenjagenScoreApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,11 @@ namespace HartenjagenScoreApp
         private int _draaiScore = 40;
         private int _rondeScore = 15;
         private string _spelStatus = "Brengen";
+        private Ronde _laatsteRonde;
 
         public int DraaiScore { get => _draaiScore; set => _draaiScore = value; }
         public int RondeScore { get => _rondeScore; set => _rondeScore = value; }
         public string SpelStatus { get => _spelStatus; set => _spelStatus = value; }
+        public Ronde LaatsteRonde { get => _laatsteRonde; set => _laatsteRonde = value; }
     }
 }
---
+++ HartenjagenScoreApp/Classes/Ronde.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HartenjagenScoreApp.Classes
+{
+    class Ronde
+    {
+        private int _rondeNummer = 0;
+        private int _rondeScore = 0;
+
+        public int RondeNummer { get => _rondeNummer; set => _rondeNummer = value; }
+        public int RondeScore { get => _rondeScore; set => _rondeScore = value; }
+    }
+}
